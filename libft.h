3a25646adf501d67be6e0b3ea6715f967d633d0e
Toufik Khattara <tkhattar@e1z3r7p15.42.us.org>
2019-10-14 21:53:19 -0700

    there is no way you dont accept it

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tkhattar <tkhattar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 21:03:45 by tkhattar          #+#    #+#             */
-/*   Updated: 2019/10/14 21:50:47 by tkhattar         ###   ########.fr       */
+/*   Updated: 2019/10/14 21:52:52 by tkhattar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,4 +82,5 @@ void ft_lstadd(t_list **alst, t_list *new);
 void ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void    ft_lstrev(t_list **alst);
+size_t	ft_lstcount(t_list *lst);
 #endif
